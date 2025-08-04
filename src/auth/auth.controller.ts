e8ef8eed6f7423a607ab11e41974ba7b1a3d6097
Garik <garikmartikyan03@gmail.com>
2025-08-04 16:10:30 +0400

    auth login error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,18 +3,19 @@ import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 
-@ApiTags('Authentication')
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
   login(@Body() userDto: CreateUserDto) {
-    // return this.authService.login(userDto);
+    console.log(userDto);
+    return this.authService.login(userDto);
   }
 
   @Post('registration')
   registration(@Body() userDto: CreateUserDto) {
-    // return this.authService.registration(userDto);
+    return this.authService.registration(userDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './users.model';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -14,12 +14,27 @@ export class UsersService {
   async createUser(dto: CreateUserDto) {
     const user = await this.userModel.create(dto);
     const role = await this.rolesService.getRoleByValue('USER');
-    await user.$set('roles', [role!.id]);
+    if (!role) {
+      throw new HttpException(
+        'Role not found',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    await user.$set('roles', [role.id]);
+    user.roles = [role];
     return user;
   }
 
   async getAllUsers() {
     const users = await this.userModel.findAll({ include: { all: true } });
     return users;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userModel.findOne({
+      where: { email },
+      include: { all: true },
+    });
+    return user;
   }
 }
